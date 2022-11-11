35b7e7b040f1b0ef7ef7d888e25488dc28223070
alpha8eta <alphabeta@nymhq.com>
2022-11-11 09:57:09 -0500

    clean up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,17 +91,17 @@ const config = {
           {
             title: 'Community',
             items: [
-              {
+              /*{
                 label: 'Stack Overflow',
                 href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
               {
                 label: 'Discord',
                 href: 'https://discordapp.com/invite/docusaurus',
-              },
+              },*/
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/nym_hq',
               },
             ],
           },
@@ -110,16 +110,16 @@ const config = {
             items: [
               {
                 label: 'Blog',
-                to: '/blog',
+                to: 'https://blog.nymhq.com',
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/nym-hq',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Nym HQ. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
